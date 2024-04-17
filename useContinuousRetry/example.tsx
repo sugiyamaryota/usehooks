cf5cad5c5c18a3127050fdc7d807dec1f26d4e54
sugiyamaryota <guqeeky6va8@gmail.com>
2024-04-17 20:22:20 +0900

    useContinuousRetry

---
+++ useContinuousRetry/example.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { useContinuousRetry } from '@uidotdev/usehooks';
+
+export default function App() {
+    const [count, setCount] = React.useState(0);
+    const hasResolved = useContinuousRetry(() => {
+        console.log("retrying");
+        return count > 10;
+    }, 1000)
+    return(
+        <section>
+            <h1>useContinuousRetry</h1>
+            <button className="primary" onClick={() => setCount(count + 1)}>
+                {count}
+            </button>
+            <pre>{JSON.stringify({hasResolved, count}, null, 2)}</pre>
+        </section>
+    )
+}
