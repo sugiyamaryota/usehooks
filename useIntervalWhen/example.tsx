6ba1e070a87e559c397eb7422a4594fd1ee0fb83
sugiyamaryota <guqeeky6va8@gmail.com>
2024-04-23 18:46:45 +0900

    useIntervalWhen

---
+++ useIntervalWhen/example.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { useIntervalWhen } from "@uidotdev/usehooks";
+
+export default function App() {
+    const [count, setCount] = React.useState(0);
+    const [when, setWhen] = React.useState(false);
+    useIntervalWhen(() =>{
+        setCount((c) => c + 0.1);
+    },{ms: 100, when, startImmediately: true}
+    )
+    return (
+        <section>
+            <h1>useIntervalWhen</h1>
+            <button title="Click to toggle the timer" onClick={() => setWhen(!when)}>
+                {count.toLocaleString("en-US", {
+                    maximumFractionDigits: 2,
+                    minimumFractionDigits: 2,
+                })}
+                <span className="btn link">{when ? "stop" : "start"}</span>
+            </button>
+        </section>
+    )
+}
